7fa6a68af3d0c66d0579eab32d6f2816fc74ca06
black32h <blackspetya@gmail.com>
2024-11-04 22:06:30 +0200

    Update Student.java1

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,32 @@
-package org.example;public class Student {
+package org.example;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int grade;
+
+    public Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    // Геттери для доступу до полів
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    // Метод для зручного виведення об'єкта
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', grade=" + grade + "}";
+    }
 }
