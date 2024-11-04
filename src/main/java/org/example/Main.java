df59f06c5f7988dc9a7627e62a5f1dca5bff56c0
black32h <blackspetya@gmail.com>
2024-11-04 22:06:30 +0200

    Update Student.java22

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,24 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main ( String[] args ) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+        try (FileInputStream fileIn = new FileInputStream("student.ser");
+             ObjectInputStream in = new ObjectInputStream(fileIn)) {
+
+            // Десеріалізація об'єкта
+            Student student = (Student) in.readObject();
+
+            // Виведення об'єкта на екран
+            System.out.println("Десеріалізований об'єкт: " + student);
+            System.out.println("Ім'я студента: " + student.getName());
+            System.out.println("Оцінка  студента: " + student.getGrade());
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,35 @@
-package org.example;public class Student {
+package org.example;
+
+import java.io.Serializable;
+
+class Student implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private String name;
+    private int grade;
+    private transient String password; // поле не серіалізується
+
+    public Student(String name, int grade, String password) {
+        this.name = name;
+        this.grade = grade;
+        this.password = password;
+    }
+
+    // Геттери для доступу до полів
+    public String getName() {
+        return name;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Метод для зручного виведення об'єкта
+    @Override
+    public String toString() {
+        return "Student{name='" + name + "', grade=" + grade + ", password='" + password + "'}";
+    }
 }
