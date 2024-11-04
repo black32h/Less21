7a805fad368db02f8ed5ca09e5c9a14cbc109bc2
black32h <blackspetya@gmail.com>
2024-11-04 22:06:30 +0200

    Update Student.java77

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,39 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main ( String[] args ) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.Student;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.io.*;
+public class Main{
+
+public static void main( String[] args) {
+    // Створюємо об'єкти Student
+    Student student1 = new Student("Alice", 90);
+    Student student2 = new Student("Bob", 80);
+
+    // Серіалізуємо об'єкт student1 у файл
+    try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("student.ser"))) {
+        oos.writeObject(student1);
+        System.out.println("Об'єкт Student серіалізовано: " + student1);
+    } catch (IOException e) {
+        e.printStackTrace();
     }
+
+    // Десеріалізуємо об'єкт student1 з файлу
+    try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("student.ser"))) {
+        Student deserializedStudent = (Student) ois.readObject();
+        System.out.println("Десеріалізований об'єкт Student: " + deserializedStudent);
+    } catch (IOException | ClassNotFoundException e) {
+        e.printStackTrace();
+    }
+
+    // Порівнюємо student1 і student2
+    int comparisonResult = student1.compareTo(student2);
+    if (comparisonResult > 0) {
+        System.out.println(student1.getName() + " має кращий результат, ніж " + student2.getName());
+    } else if (comparisonResult < 0) {
+        System.out.println(student2.getName() + " має кращий результат, ніж " + student1.getName());
+    } else {
+        System.out.println(student1.getName() + " і " + student2.getName() + " мають однаковий результат");
+    }
+}
 }
--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -1,2 +1,36 @@
-package org.example;public class Student {
+package org.example;
+
+import java.io.*;
+
+public class Student implements Serializable, Comparable<Student> {
+    private static final long serialVersionUID = 1L; // Унікальний ідентифікатор серіалізації
+
+    private String name;
+    private final int grade;
+
+    public Student ( String name, int grade ) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    // Геттери для полів
+    public String getName () {
+        return name;
+    }
+
+    public int getGrade () {
+        return grade;
+    }
+
+    // Метод порівняння за полем grade
+    @Override
+    public int compareTo ( Student other ) {
+        return Integer.compare(this.grade, other.grade); // Порівнює за grade
+    }
+
+    @Override
+    public String toString () {
+        return "Student{name='" + name + "', grade=" + grade + "}";
+    }
 }
+
